a38ecee12172d121ab0ed75c5704db5df568a5dd
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-03-01 05:02:53 +0200

    Add Define an Interface IPerson Exercise

---
+++ Exercise - Interfaces and Abstraction/01. Define an Interface IPerson/Code/IPerson.cs
@@ -0,0 +1,6 @@
+namespace PersonInfo;
+
+public interface IPerson {
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
---
+++ Exercise - Interfaces and Abstraction/01. Define an Interface IPerson/Code/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PersonInfo;
+
+public class StartUp {
+    public static void Main(string[] args) {
+        IPerson person = new Citizen(Console.ReadLine(), int.Parse(Console.ReadLine()));
+        Console.WriteLine(person.Name);
+        Console.WriteLine(person.Age);
+    }
+}
