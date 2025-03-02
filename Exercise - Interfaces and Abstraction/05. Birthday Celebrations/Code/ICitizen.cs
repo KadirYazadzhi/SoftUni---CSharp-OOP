a7ec87d311ca26e642cbc56530721a6c489fb64f
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-03-02 08:11:34 +0200

    Create ICitizen.cs

---
+++ Exercise - Interfaces and Abstraction/05. Birthday Celebrations/Code/ICitizen.cs
@@ -0,0 +1,8 @@
+namespace BirthdayCelebrations;
+
+public interface ICitizen {
+    public string Name { get; }
+    public int Age { get; }
+    public string Id { get; }
+    public string Birthday { get; }
+}
