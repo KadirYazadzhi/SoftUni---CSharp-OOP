c6715c9f4e2ed7d6deed85acdbfdd6d8f7c9d36d
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-03-12 03:07:15 +0200

    Add Vehicles Extension Exercise

---
+++ Exercise - Polymorphism /02. Vehicles Extension/Code/Truck.cs
@@ -0,0 +1,11 @@
+namespace Vehicles;
+
+public class Truck : BaseVehicle {
+    public Truck(double fuelQuantity, double fuelConsumption, double tankCapacity) : base(fuelQuantity, fuelConsumption, tankCapacity) {
+    }
+
+    protected override double ConsumptionIncrease => 1.6;
+
+    public override bool Refuel(double liters) => base.Refuel(liters * 0.95);
+    
+}
