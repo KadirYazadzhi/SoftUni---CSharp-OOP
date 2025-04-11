795b72e0c07f999557a732498136fb92f96c6d56
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-04-11 13:39:22 +0300

    Add Exercise

---
+++ Exercise - Reflection and Attributes/01. Command Pattern/Code/Core/Commands/ExitCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace CommandPattern.Core.Commands;
+
+using CommandPattern.Core.Contracts;
+
+public class ExitCommand : ICommand {
+    public string Execute(string[] args) {
+        Environment.Exit(0);
+        return string.Empty;
+    }
+}
